ffcafb32dcc12d9bef7ccca4b1cc8e9cc5006261
Rafael Robles <gdl.villber@gmail.com>
2018-08-13 19:41:37 -0500

    cambio de version, ajustes no imagenes

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -33,7 +33,7 @@ document.getElementById('boton_apoyame').innerHTML = '¿Desea apoyar la app?';
     if (fondo === 'universo') {
         var images = ['uni1.jpg', 'uni2.jpg', 'uni3.jpg', 'uni4.jpg', 'uni5.jpg', 'uni6.jpg', 'uni7.jpg', 'uni8.jpg', 'uni9.jpg', 'uni10.jpeg', 'uni11.jpeg', 'uni12.jpeg'];
         (function($){
-            $('.himno').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
+            $('.fondo').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'color': '#fff'});
             $('.himno .azul, .himno .nblu').css({'color': '#62b0ff'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'text-shadow': '2px 2px 2px rgba(0, 0, 0, 1)'});
@@ -43,7 +43,7 @@ document.getElementById('boton_apoyame').innerHTML = '¿Desea apoyar la app?';
     if (fondo === 'ldm') {
         var images = ['ldm1.jpg', 'ldm2.jpg', 'ldm3.jpg', 'ldm4.jpg', 'ldm5.jpg', 'ldm6.jpg', 'ldm7.jpg'];
         (function($){
-            $('.himno').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
+            $('.fondo').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'text-shadow': '2px 2px 2px rgba(255, 255, 255, 1)'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left, .himno .nblu').css({'font-size': letra_tamano});
         })(jQuery);
@@ -51,7 +51,7 @@ document.getElementById('boton_apoyame').innerHTML = '¿Desea apoyar la app?';
     if (fondo === 'nat') {
         var images = ['nat1.jpg', 'nat2.jpg', 'nat3.jpg', 'nat4.jpg', 'nat5.jpg', 'nat6.jpg', 'nat7.jpg', 'nat8.jpg'];
         (function($){
-            $('.himno').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
+            $('.fondo').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'text-shadow': '2px 2px 2px rgba(255, 255, 255, 1)'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left, .himno .nblu').css({'font-size': letra_tamano});
         })(jQuery);
@@ -59,7 +59,7 @@ document.getElementById('boton_apoyame').innerHTML = '¿Desea apoyar la app?';
     if (fondo === 'claro') {
         var images = ['liso1.jpg', 'liso2.jpg', 'liso3.jpg', 'liso4.jpg', 'liso5.jpg', 'liso6.jpg', 'liso7.jpg', 'liso8.jpg'];
         (function($){
-            $('.himno').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
+            $('.fondo').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'text-shadow': '2px 2px 2px rgba(255, 255, 255, 1)'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left, .himno .nblu').css({'font-size': letra_tamano});
         })(jQuery);
@@ -67,7 +67,7 @@ document.getElementById('boton_apoyame').innerHTML = '¿Desea apoyar la app?';
     if (fondo === 'text') {
         var images = ['text1.jpg', 'text2.jpg', 'text3.jpg', 'text4.jpg', 'text5.jpg', 'text6.jpg', 'text7.jpg', 'text8.jpg'];
         (function($){
-            $('.himno').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
+            $('.fondo').css({'background-image': 'url(img/fondos/' + images[Math.floor(Math.random() * images.length)] + ')'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left').css({'text-shadow': '2px 2px 2px rgba(255, 255, 255, 1)'});
             $('.himno .cnt, .himno i, .himno .just, .himno .right, .himno .left, .himno .nblu').css({'font-size': letra_tamano});
         })(jQuery);
